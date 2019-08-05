0b44a501fb6ca37bba5c121d00e62f7c7ef09d46
seanauff <sean.auffinger@gmail.com>
2019-08-05 10:54:42 -0400

    add WOL lib

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import paho.mqtt.client as mqtt
+from wakeonlan import send_magic_packet
 
 # read in needed env variables
 MQTT_BROKER_HOST  = os.environ['MQTT_BROKER_HOST']
@@ -13,15 +14,19 @@ MQTT_TOPIC_PREFIX = os.environ['MQTT_TOPIC_PREFIX']
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    
+
 # set up mqtt client
 client = mqtt.Client(client_id=MQTT_CLIENT_ID)
 if MQTT_USERNAME and MQTT_PASSWORD:
     client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
-client.on_connect = on_connect
+client.on_connect = on_connect # on connect callback
+client.on_message = on_message # on message callback
 
 # connect to broker
 client.connect(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT)
 client.loop_start()
 client.publish(MQTT_TOPIC_PREFIX+"/status","Online")
+
+# subscribe to command topic
+client.subscribe(MQTT_TOPIC_PREFIX+"/command")
 
