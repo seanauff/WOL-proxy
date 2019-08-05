790c5201ac86e644c3b326e695e485b7e7320bf7
seanauff <sean.auffinger@gmail.com>
2019-08-05 19:05:14 -0400

    added debug prints

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -11,28 +11,34 @@ MQTT_USERNAME      = os.environ['MQTT_USERNAME']
 MQTT_PASSWORD      = os.environ['MQTT_PASSWORD']
 MQTT_TOPIC_PREFIX  = os.environ['MQTT_TOPIC_PREFIX']
 WOL_BROADCAST_ADDR = os.environ['WOL_BROADCAST_ADDR']
+print("All env vars read.")
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print(f"Connected with result code {rc}")
 
 # callback for when the client receives a message on the subscribed topic
 def on_message(client, userdata, message):
     send_magic_packet(message.payload,ip_address=WOL_BROADCAST_ADDR)
+    print(f"Magic packet sent to {message.payload}")
 
 # set up mqtt client
 client = mqtt.Client(client_id=MQTT_CLIENT_ID)
 if MQTT_USERNAME and MQTT_PASSWORD:
     client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
+    print("Username and password set.")
 client.on_connect = on_connect # on connect callback
 client.on_message = on_message # on message callback
 
 # connect to broker
 client.connect(MQTT_BROKER_HOST, port=int(MQTT_BROKER_PORT))
 client.publish(MQTT_TOPIC_PREFIX+"/status","Online")
+print(f"Connected to broker at {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT}")
 
 # subscribe to command topic
 client.subscribe(MQTT_TOPIC_PREFIX+"/command")
+print(f"Subcribed to commands on topic {MQTT_TOPIC_PREFIX}/command")
 
 # start loop
 client.loop_start()
+print(f"Starting loop.")
