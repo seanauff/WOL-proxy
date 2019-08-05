202569e7c70e173df9ef5e2a211f73010084ecc9
seanauff <sean.auffinger@gmail.com>
2019-08-05 10:44:12 -0400

    set up and connect to broker

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -14,3 +14,14 @@ MQTT_USERNAME     = os.environ['MQTT_USERNAME']
 MQTT_PASSWORD     = os.environ['MQTT_PASSWORD']
 MQTT_TOPIC_PREFIX = os.environ['MQTT_TOPIC_PREFIX']
 
+# set up mqtt client
+client = mqtt.Client(client_id=MQTT_CLIENT_ID)
+if MQTT_USERNAME and MQTT_PASSWORD:
+    client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
+client.on_connect = on_connect
+
+# connect to broker
+client.connect(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT)
+client.loop_start()
+client.publish(MQTT_TOPIC_PREFIX+"/status","Online")
+
