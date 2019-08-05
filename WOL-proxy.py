d73a41d7fc10109c7017bd3c86a27f9165e01193
seanauff <sean.auffinger@gmail.com>
2019-08-05 11:24:48 -0400

    add rest of needed instructions

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -4,12 +4,13 @@ import paho.mqtt.client as mqtt
 from wakeonlan import send_magic_packet
 
 # read in needed env variables
-MQTT_BROKER_HOST  = os.environ['MQTT_BROKER_HOST']
-MQTT_BROKER_PORT  = os.environ['MQTT_BROKER_PORT']
-MQTT_CLIENT_ID    = os.environ['MQTT_CLIENT_ID']
-MQTT_USERNAME     = os.environ['MQTT_USERNAME']
-MQTT_PASSWORD     = os.environ['MQTT_PASSWORD']
-MQTT_TOPIC_PREFIX = os.environ['MQTT_TOPIC_PREFIX']
+MQTT_BROKER_HOST   = os.environ['MQTT_BROKER_HOST']
+MQTT_BROKER_PORT   = os.environ['MQTT_BROKER_PORT']
+MQTT_CLIENT_ID     = os.environ['MQTT_CLIENT_ID']
+MQTT_USERNAME      = os.environ['MQTT_USERNAME']
+MQTT_PASSWORD      = os.environ['MQTT_PASSWORD']
+MQTT_TOPIC_PREFIX  = os.environ['MQTT_TOPIC_PREFIX']
+WOL_BROADCAST_ADDR = os.environ['WOL_BROADCAST_ADDR']
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -24,12 +25,14 @@ client.on_message = on_message # on message callback
 
 # connect to broker
 client.connect(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT)
-client.loop_start()
 client.publish(MQTT_TOPIC_PREFIX+"/status","Online")
 
 # subscribe to command topic
 client.subscribe(MQTT_TOPIC_PREFIX+"/command")
 
-# callback for when the client receives a message on the subcribed topic
+# callback for when the client receives a message on the subscribed topic
 def on_message(client, userdata, message):
-    
+    send_magic_packet(message.payload,ip_address=WOL_BROADCAST_ADDR)
+
+# start loop
+client.loop_start()
