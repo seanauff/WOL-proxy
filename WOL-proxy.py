99e600bca86fb22c2751fcbf14dd11a6d8aaf3bc
seanauff <sean.auffinger@gmail.com>
2019-08-05 15:59:19 -0400

    cast port to int

--- WOL-proxy.py
+++ WOL-proxy.py
@@ -5,7 +5,7 @@ from wakeonlan import send_magic_packet
 
 # read in needed env variables
 MQTT_BROKER_HOST   = os.environ['MQTT_BROKER_HOST']
-MQTT_BROKER_PORT   = os.environ['MQTT_BROKER_PORT']
+MQTT_BROKER_PORT   = int(os.environ['MQTT_BROKER_PORT'])
 MQTT_CLIENT_ID     = os.environ['MQTT_CLIENT_ID']
 MQTT_USERNAME      = os.environ['MQTT_USERNAME']
 MQTT_PASSWORD      = os.environ['MQTT_PASSWORD']
