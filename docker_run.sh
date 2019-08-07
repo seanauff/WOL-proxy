d171a8c4f3f10cc95362e384f381704c9942af10
seanauff <sean.auffinger@gmail.com>
2019-08-07 15:07:13 -0400

    remove docker run script

--- docker_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# basic run command, uses all default env vars
-docker run -d --net=host seanauff/wol-proxy
-
-# command to set env vars to custom values
-# docker run -d --net=host -e 
